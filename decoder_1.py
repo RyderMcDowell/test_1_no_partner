0a71ab6252337cfe7b43f264e605d6d09f91be9a
Ryder McDowell <rydermcdowell@omglookvidlynch.lan>
2023-03-08 17:41:05 -0500

    commit w/o decoder

--- decoder_1.py
+++
@@ -1,9 +0,0 @@
-def decode(encodedPassword):
-    decodedPassword = " "
-    for i in range(8):
-        addedChar = str(int(encodedPassword[i])+3*-1)
-        decodedPassword = decodedPassword + addedChar
-    decodedPassword = decodedPassword.lstrip()
-    print(decodedPassword)
-    print(f"The encoded password is {encodedPassword}, and the original password is {decodedPassword}.")
-    print(" ")
